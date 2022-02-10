2bcdfd7b241713eb7a49ba64d73378a2eafff867
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-10 14:36:17 +0900

    ex02

---
+++ Ex02_02.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Ex02_02 {
+	public static void main (String[]args){
+	
+	int suryo,tanka;
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("”—Ê‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢>");
+	tanka = scanner.nextInt();
+	
+	System.out.print("’P‰¿‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢>");
+	suryo = scanner.nextInt();
+	
+	System.out.print("‹àŠz‚Í" + suryo * tanka + "‚Å‚·");
+	
+	
+	}
+	}
+	
