7684847d60454372eca890c353d0c1ec34569ff2
honghong <90856538+H-H-C@users.noreply.github.com>
2022-02-10 14:36:04 +0900

    ex01

---
+++ Ex02_01.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Ex02_01 {
+	 public static void main(String[] args){
+	 
+	 
+	 	 String kamokuname,name;
+		Scanner scanner = new Scanner(System.in);	 
+	 
+	   	 System.out.print("‰È–Ú–¼‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢>");
+		 kamokuname = scanner.next();
+		 
+   		 System.out.print("–¼‘O‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢>");
+   		 name = scanner.next();
+	 
+		 System.out.print("‚ ‚È‚½‚Í" + kamokuname + "‚Ì" + name + "‚³‚ñ‚Å‚·");
+	 
+	 
+	
+	 
+	 	}
+	 	}
